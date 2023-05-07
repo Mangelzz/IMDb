578b1c454daa60b222e715d9e55d443199d2cba0
Mangelzz <mangel.grzz@gmail.com>
2023-05-07 11:58:18 -0500

    Create the about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export const Header = () => {
         <div className='flex items-center space-x-5'>
             <DarkModeSwitch />
             <Link href="/">
-              <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMBb</span><span className='text-xl hidden sm:inline'>Clone</span></h2>
+              <h2 className='text-2xl'><span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>IMDb</span><span className='text-xl hidden sm:inline'>Clone</span></h2>
             </Link>
         </div>
     </div>
