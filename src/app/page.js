d31689bc237dfab5ac8faa42e5e4f4805cbb6e56
Mangelzz <mangel.grzz@gmail.com>
2023-05-10 18:22:50 -0500

    Fetch data from TMDB Api in the Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
-export default function Home() {
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US`, { next: { revalidate: 10000}});
+
+  if(!res.ok){
+    throw new Error("Something went wrong");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  
   return (
-    <h1 className="text-red-500">HOME</h1>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
